05656b021ab2aa9e06d4ef2b725c49c2e37f08df
Mikael Samvelian <mikael.samvelian@gmail.com>
2020-02-03 20:39:36 -0500

    Add test lines at the bottom

--- dfs.py
+++ dfs.py
@@ -1,12 +1,10 @@
 import numpy as np
-graph = {
-    '0 0 0 0 0 0 1 0 0 1 1 1 0 0 1 0': [np.array([0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]), np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])],
-    '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0': []
-}
 
 def DFS(graph, start, goal, explored, path_so_far):
     # Returns path from start to goal in graph as a string
     explored.append(start)
+    print(f'Visited State: {start}')
+    print(f'Closed List: {explored}')
     if start == goal:
         return path_so_far + ' -> ' + start
     if start in graph:
@@ -20,5 +18,12 @@ def DFS(graph, start, goal, explored, path_so_far):
                   return p
     return ""
 
-dfs_solution = DFS(graph, '0 0 0 0 0 0 1 0 0 1 1 1 0 0 1 0', '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0', [], "")
-print(dfs_solution)
+
+# graph = {
+#     '0 0 0 0 0 0 1 0 0 1 1 1 0 0 1 0': [np.array([0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]), np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])],
+#     '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0': []
+# }
+
+# dfs_solution = DFS(graph, '0 0 0 0 0 0 1 0 0 1 1 1 0 0 1 0', '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0', [], "")
+
+# print(dfs_solution)
