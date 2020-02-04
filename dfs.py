3a7d89118d2b127c1a484ef659d7f65aad40accf
Mikael Samvelian <mikael.samvelian@gmail.com>
2020-02-03 19:59:25 -0500

    Fix dfs to work with cases where the nodes do not have any children

--- dfs.py
+++ dfs.py
@@ -1,6 +1,6 @@
 import numpy as np
 graph = {
-    '0 0 0 0 0 0 1 0 0 1 1 1 0 0 1 0': [np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])],
+    '0 0 0 0 0 0 1 0 0 1 1 1 0 0 1 0': [np.array([0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]), np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])],
     '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0': []
 }
 
@@ -9,14 +9,15 @@ def DFS(graph, start, goal, explored, path_so_far):
     explored.add(start)
     if start == goal:
         return path_so_far + ' -> ' + start
-    for w in graph[start]:
-        if str(w).strip('[]') not in explored:
-            if(path_so_far is not ""):
-              p = DFS(graph, str(w).strip('[]'), goal, explored, path_so_far + ' -> ' + start)
-            else:
-              p = DFS(graph, str(w).strip('[]'), goal, explored, start)
-            if p:
-                return p
+    if start in graph:
+      for w in graph[start]:
+          if str(w).strip('[]') not in explored:
+              if(path_so_far is not ""):
+                p = DFS(graph, str(w).strip('[]'), goal, explored, path_so_far + ' -> ' + start)
+              else:
+                p = DFS(graph, str(w).strip('[]'), goal, explored, start)
+              if p:
+                  return p
     return ""
 
 dfs_solution = DFS(graph, '0 0 0 0 0 0 1 0 0 1 1 1 0 0 1 0', '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0', set(), "")
