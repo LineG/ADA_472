de00adc347dd5911d9184a2569bca51a47c1f07c
LineG <ghanemline@gmail.com>
2020-02-08 19:13:23 -0500

    graph generation node by node as needed

--- dfs.py
+++ dfs.py
@@ -1,44 +1,69 @@
 import numpy as np
-from next_moves import generate_graph,flip_adjacent
+from next_moves import generate_graph, flip_adjacent,generate_next_moves
+import copy
 
-def DFS(graph, start, goal, explored, path_so_far):
+MAX_D = 9
+NN= 4
+
+
+b = np.array([0,0,0,0,1,1,0,1,0,0,1,0,1,0,0,1])
+goal ='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
+
+# b = np.array([1,1,1,0,0,1,0,1,1])
+# goal ='0 0 0 0 0 0 0 0 0'
+
+
+def children_node(graph,b):
+    children = np.array([])
+
+    children = generate_next_moves(b,NN)
+    board = children
+
+    graph[str(b).strip('[]')] = children
+    for child in children:
+        if not (str(child).strip('[]') in graph):
+            graph[str(child).strip('[]')] = []
+
+
+def DFS(graph, start, goal, explored, path_so_far,level):
     # Returns path from start to goal in graph as a string
+    if not level > MAX_D:
+        children_node(graph,start)
+    print("generating nodes on level ", level)
+    start = str(start).strip('[]')
     explored.append(start)
-    # print(f'Visited State: {start}')
-    # print(f'Closed List: {explored}')
-    if start == goal:
-        return path_so_far + ' -> ' + start
 
-    if graph[start] == []:
-        print(path_so_far + ' -> ' + start)
+    if level > MAX_D:
+        return ""
+
+    if start == goal:
+        return path_so_far + ' -> level ' + f'{level}'+' -- '+start
 
     if start in graph:
       for w in graph[start]:
-          if str(w).strip('[]') not in explored:
+          str_w = str(w).strip('[]')
+          if str_w not in explored:
               if(path_so_far is not ""):
-                p = DFS(graph, str(w).strip('[]'), goal, explored, path_so_far + ' -> ' + start)
+                p = DFS(graph, w, goal, explored,f'{path_so_far} -> level {level} -- {start}',level+1)
               else:
-                p = DFS(graph, str(w).strip('[]'), goal, explored, start)
+                p = DFS(graph, w, goal, explored, start,level)
               if p:
-                  return p
+                return p
+
     return ""
 
-
-# graph = {
-#     '0 1 0 1 1 1 0 1 0': [np.array([0,0,0,0,0,1,0,0,0]),np.array([0,0,0,1,0,1,1,0,1])],
-#     '0 0 0 0 0 1 0 0 0': [],
-#     '0 0 0 1 0 1 1 0 1' : [np.array([0,0,0,0,0,0,0,0,0])]
-# }
-# dfs_solution = DFS(graph,'0 1 0 1 1 1 0 1 0','0 0 0 0 0 0 0 0 0',[],"")
-
-# b = np.array([0,1,0,0,0,0,1,1,0])
-b = np.array([1,0,1,0,0,0,0,0,1,1,1,1,1,0,1,0])
+def start_DFS(b, goal):
+    g = {}
+    # graph(g,b)
+    p = DFS(g,b,goal,[],"",1)
+    return p
 
 
-graph = generate_graph(b,5,4)
-# for c in graph:
-#     print(c)
-#     print(graph[c])
-# print(graph)
-dfs_solution = DFS(graph,str(b).strip('[]'),'0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0',[],"")
-print(dfs_solution)
+
+dfs_solution = start_DFS(b,goal)
+
+
+f = open("test.txt","w+")
+
+f.write(dfs_solution)
+f.close()
