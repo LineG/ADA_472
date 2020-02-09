98d5991b3e6db154d18303a1464c1390876669a6
LineG <ghanemline@gmail.com>
2020-02-09 08:03:12 -0500

    some samll changes

--- dfs.py
+++ dfs.py
@@ -2,15 +2,15 @@ import numpy as np
 from next_moves import generate_graph, flip_adjacent,generate_next_moves
 import copy
 
-MAX_D = 9
-NN= 4
+MAX_D = 7
+NN= 3
 
 
-b = np.array([0,0,0,0,1,1,0,1,0,0,1,0,1,0,0,1])
-goal ='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
+# b = np.array([0,0,0,0,1,1,0,1,0,0,1,0,1,0,0,1])
+# goal ='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
 
-# b = np.array([1,1,1,0,0,1,0,1,1])
-# goal ='0 0 0 0 0 0 0 0 0'
+b = np.array([1,1,1,0,0,1,0,1,1])
+goal ='0 0 0 0 0 0 0 0 0'
 
 
 def children_node(graph,b):
@@ -20,6 +20,7 @@ def children_node(graph,b):
     board = children
 
     graph[str(b).strip('[]')] = children
+
     for child in children:
         if not (str(child).strip('[]') in graph):
             graph[str(child).strip('[]')] = []
@@ -29,7 +30,7 @@ def DFS(graph, start, goal, explored, path_so_far,level):
     # Returns path from start to goal in graph as a string
     if not level > MAX_D:
         children_node(graph,start)
-    print("generating nodes on level ", level)
+        print("generating nodes on level ", level)
     start = str(start).strip('[]')
     explored.append(start)
 
@@ -37,14 +38,14 @@ def DFS(graph, start, goal, explored, path_so_far,level):
         return ""
 
     if start == goal:
-        return path_so_far + ' -> level ' + f'{level}'+' -- '+start
+        return path_so_far + ' -> level ' + f'{level+1}'+' -- '+start
 
     if start in graph:
       for w in graph[start]:
           str_w = str(w).strip('[]')
           if str_w not in explored:
               if(path_so_far is not ""):
-                p = DFS(graph, w, goal, explored,f'{path_so_far} -> level {level} -- {start}',level+1)
+                p = DFS(graph, w, goal, explored,f'{path_so_far} -> level {level+1} -- {start}',level+1)
               else:
                 p = DFS(graph, w, goal, explored, start,level)
               if p:
@@ -54,7 +55,6 @@ def DFS(graph, start, goal, explored, path_so_far,level):
 
 def start_DFS(b, goal):
     g = {}
-    # graph(g,b)
     p = DFS(g,b,goal,[],"",1)
     return p
 
