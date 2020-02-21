cc28be2ad7fbabb0b26b6a95ea5110d59fba3f6e
LineG <ghanemline@gmail.com>
2020-02-21 00:04:22 -0500

    aStart algorithm tested only on a 3 * 3 and 4 * 4 but it should work for more

--- bfs.py
+++ bfs.py
@@ -1,3 +1,7 @@
+# Line Ghanem 27280076
+# Anthony Iatropoulos 40028246
+# Mikael Samvelian 40003178
+
 def getNodeVal(node) -> int:
 	"""Function that converts a node to an integer.
 	Ex: [[0,0,0],[1,0,1],[0,1,0]] -> 1*0 + 2*0 + 4*0 + 8*1 + 16*0 + 32*1 + 64*0 + 128*1 + 256*0 -> 168
